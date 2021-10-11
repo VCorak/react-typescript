53827c5f3b3158642bab068f9bd9ea34acfb512a
Valentina Corak <valentinacorak@gmail.com>
2021-10-11 15:57:41 +0200

    Interface in Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+interface IState {
+    books: {
+        title: string
+        url: string
+        score: number
+        author: string
+        note?: string
+    }[]
+}
+
 function App() {
+
+    const [books, setBooks] = useState<IState["books"]>([])
+
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<h1>Books to Read</h1>
     </div>
   );
 }
