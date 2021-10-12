d60ec20578ee1572e5c6a3450784796a5eb347d3
Valentina Corak <valentinacorak@gmail.com>
2021-10-12 10:40:06 +0200

    Add to list inputs are working

--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -1,36 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddToList = () => {
+    
+    const [input, setInput]= useState({
+        title:"",
+        author:"",
+        img:"",
+        score:"",
+        note:""
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value // overriding the input name with written value
+        })
+
+        // with the "void" we are returning nothing in this function
+    }
+
+    const handleClick = () => {
+
+    })
+
     return (
         <div className="AddToList">
             <input
                 type="text"
                 placeholder="Title"
                 className="AddToList-input"
+                value={input.title} // two way binding typical React thing
+                onChange={handleChange}
+                name="title"
             />
             <input
                 type="text"
                 placeholder="Author"
                 className="AddToList-input"
+                value={input.author}
+                onChange={handleChange}
+                name="author"
             />
             <input
                 type="text"
                 placeholder="Image Url"
                 className="AddToList-input"
+                value={input.img}
+                onChange={handleChange}
+                name="img"
             />
             <input
                 type="text"
                 placeholder="Score"
                 className="AddToList-input"
+                value={input.score}
+                onChange={handleChange}
+                name="score"
             />
-            <input
-                type="text"
+            <textarea
                 placeholder="Note"
                 className="AddToList-input"
+                value={input.note}
+                onChange={handleChange}
+                name="note"
             />
+            <button
+                className="AddToList-btn"
+                onClick={handleClick}
+
+            >
+
+                Add to List</button>
 
         </div>
     );
-};
+}
 
 export default AddToList;
